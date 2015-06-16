660fa923a933122a0a2703040b7a7e6caa1dfac0
Rudy <drawncloser@gmail.com>
2015-06-15 21:13:46 -0500

    Initial

--- Brocfile.js
+++ Brocfile.js
@@ -19,5 +19,4 @@ var app = new EmberApp();
 app.import('bower_components/bootstrap/dist/css/bootstrap.css');
 app.import('bower_components/bootstrap/dist/js/bootstrap.js');
 app.import('bower_components/moment/min/moment.min.js');
-app.import('bower_components/firebase-simple-login/firebase-simple-login.js');
 module.exports = app.toTree();
